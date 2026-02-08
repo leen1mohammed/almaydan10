acb2defde785c2a194d1f4df7f4a71b80b6bab02
janaalturki <163104999+janaalturki@users.noreply.github.com>
2026-02-09 00:57:06 +0300

    delete some file

--- src/app/(public)/about/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function AboutPage() {
-  return (
-    <main className="p-6">
-      <h1 className="text-2xl font-bold">حول الميدان</h1>
-      <p className="mt-2 opacity-80">صفحة Placeholder ضمن الستاندرد.</p>
-    </main>
-  );
-}
