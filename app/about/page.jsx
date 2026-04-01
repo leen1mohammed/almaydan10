eb9395f5ecb02f64114ae9ffddfd3a964eec03e7
Leen Alnuem <162335394+leen1mohammed@users.noreply.github.com>
2026-04-01 12:25:24 +0300

    اضافة صفحة حول

--- app/about/page.jsx
+++
@@ -1,9 +0,0 @@
-export default function about() {
-  return (
-    <main className="min-h-screen bg-esport-dark">
-      <div className="flex flex-col items-center justify-center h-[50vh] text-white">
-        <h1 className="text-4xl font-bold">صفحة حول هنا</h1>
-      </div>
-    </main>
-  );
-}
